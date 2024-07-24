398c1f30b59c3a3e0ddf4974f92a45f7ac8ebe6d
Shashi Gupta <52447238+SBOLD-BOLT@users.noreply.github.com>
2024-07-24 13:55:52 +0530

    Started Angular component1

---
+++ Angular-July-Training/src/app/binding-demo/binding-demo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binding-demo',
+  templateUrl: './binding-demo.component.html',
+  styleUrls: ['./binding-demo.component.css'],
+})
+export class BindingDemoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
